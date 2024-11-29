5c0cbf667f3d0b9baaccaf27eebbcc23ca5e025a
wanglirun <changyusong123@gmail.com>
2024-11-29 01:44:45 +0100

    backup

---
+++ parallelize/matrix.h
@@ -0,0 +1,30 @@
+#ifndef MATRIX_H_HEADER
+#define MATRIX_H_HEADER
+#include<string>
+#include<iostream>
+#include<cstring>
+#include<fstream>
+#include<stdio.h>
+#include<vector>
+#include<cmath>
+#include<chrono>
+#include <numeric>
+#include<algorithm>
+#include<cuda_runtime.h>
+#define ROW 500
+#define COL 500
+#define SIZE 250000
+using namespace std;
+void ReadData(const string& filename, double* data);
+void Pearson(double* a, double* b,double* rho);
+void Add(double* a, double*b,double rho);
+void SpatialFilter(double* a);
+void AnomalyDetection(double* a);
+void writeData(const string& filename,double* data);
+
+
+
+
+
+#endif
+
