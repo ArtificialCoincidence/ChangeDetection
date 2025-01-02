2674400d8dc267ed7a4b9f35e91202decd4cfef7
wanglirun <changyusong123@gmail.com>
2025-01-02 03:43:14 +0100

    final

--- baseline/main.cpp
+++ baseline/main.cpp
@@ -9,63 +9,53 @@ int main() {
 
     //-------------------Read Data------------------------
 
-    double* testMatrix = nullptr;
-    double* refMatrix = nullptr;
-    double* finalRes = nullptr;
-    double minRho = 1.0;
+	double* testMatrix = nullptr;
+	double* refMatrix = nullptr;
+	double* finalRes = nullptr;
+	double minRho = 1.0;
 
-    testMatrix=(double*)malloc(sizeof(double)*SIZE);
-    refMatrix=(double*)malloc(sizeof(double)*SIZE);
-    finalRes=(double*)malloc(sizeof(double)*SIZE);
-  
+	testMatrix=(double*)malloc(sizeof(double)*SIZE);
+	refMatrix=(double*)malloc(sizeof(double)*SIZE);
+	finalRes=(double*)malloc(sizeof(double)*SIZE);
 
-/*    double *debug=(double*)malloc(sizeof(double)*9);
 
-    for(int i=0;i<9;++i)
-	    debug[i]=i+1;
-    SpatialFilter(debug);
-    for(int i=0;i<9;++i)
-	    printf("%f,",debug[i]);
-	writeData("./res.txt",debug);
-  */
-        ReadData(string(PATH) + "Itest6.dat",testMatrix);
+	ReadData(string(PATH) + "Itest6.dat",testMatrix);
 
 	duration<double> duration(0);
  
-    for (int i = 0; i < LAG; ++i) {
-        string path = string(PATH) + "Iref6" + string(1, 'A' + i) + ".dat";
-        ReadData(path,refMatrix);
+	for (int i = 0; i < LAG; ++i) {
+		string path = string(PATH) + "Iref6" + string(1, 'A' + i) + ".dat";
+		ReadData(path,refMatrix);
 
-	
-    	auto t1 = high_resolution_clock::now();
-        //-----------------------AR(1)-------------------------
-        double rho = Pearson(testMatrix, refMatrix);
-        //std::cout << "Pearson correlation: " << fixed << std::setprecision(10) << rho << std::endl;
-        
-	Add(refMatrix, testMatrix,rho);
-        //-----------SpatialFilter and AnomalyDetection--------
-        SpatialFilter(refMatrix);
-	
-	
-       // writeData("./res.txt",refMatrix);
-	AnomalyDetection(refMatrix);
+		
+		auto t1 = high_resolution_clock::now();
+		//-----------------------AR(1)-------------------------
+		double rho = Pearson(testMatrix, refMatrix);
+		//std::cout << "Pearson correlation: " << fixed << std::setprecision(10) << rho << std::endl;
+		
+		Add(refMatrix, testMatrix,rho);
+		//-----------SpatialFilter and AnomalyDetection--------
+		SpatialFilter(refMatrix);
+		
+		
+	       // writeData("./res.txt",refMatrix);
+		AnomalyDetection(refMatrix);
 
-        //-----------update the result--------
-        double r = Pearson(refMatrix, testMatrix);
-        if (r < minRho) {
-            minRho = r;
-            memcpy(finalRes, refMatrix, sizeof(double) * SIZE);
-        }
+		//-----------update the result--------
+		double r = Pearson(refMatrix, testMatrix);
+		if (r < minRho) {
+			minRho = r;
+			memcpy(finalRes, refMatrix, sizeof(double) * SIZE);
+		}
 
 
-    	auto t2 = high_resolution_clock::now();
-    	duration+=t2-t1;
-    }
+		auto t2 = high_resolution_clock::now();
+		duration+=t2-t1;
+	}
+	std::cout << "Compute time: " << duration.count() << " seconds" << std::endl;
 
-    std::cout << "Compute time: " << duration.count() << " seconds" << std::endl;
-
-    //-----------------------Write Data--------------------
-    //writeData("./res.txt",finalRes);
-    return 0;
+	//-----------------------Write Data--------------------
+	//writeData("./res.txt",finalRes);
+	return 0;
 }
 
